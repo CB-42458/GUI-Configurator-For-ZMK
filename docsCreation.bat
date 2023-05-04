fc16254d8772df6d741a3a000836d193a20c6624
CB-42458 <cb50243987@gmail.com>
2023-05-04 21:47:15 +0100

    Update docsCreation.bat
    I added a line which run the setup batch file because I kept for getting to reinstall the packages, which meant that the documentation didn't actually update.

--- docsCreation.bat
+++ docsCreation.bat
@@ -1,5 +1,7 @@
 :: This file is used to create the documentation for the project. using the pdoc python package.
 :: This file is not required for the project to run.
+:: it runs the setup.bat file to install the reinstall the packages.
+call setup.bat
 pip install pdoc
 rmdir /s /q docs
 pdoc ./Application/main.py ./Application/functions.py ./standard_widgets.py ZMK ListUnion pykle_serial -o ./docs
